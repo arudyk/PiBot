4b352c50ca9248e55f9e3c433e8c8ef35c9fc9ca
Andriy Rudyk <arudyk.dev@gmail.com>
2013-09-18 21:18:15 -0400

    fixed pibot.py

--- pibot_api/pibot.py
+++ pibot_api/pibot.py
@@ -4,42 +4,42 @@ class PiBot(object):
     """Class representing PiBot"""
     def __init__(self):
         self.status = None
-        power_on()
+        power_ctrl(POWER_ON)
 
     def forward(self):
         """Moves PiBot forward"""
-        motor_a(FORWARD)
-        motor_b(FORWARD)
+        motor_ctrl(MOTOR_A, FORWARD)
+        motor_ctrl(MOTOR_B,FORWARD)
         self.status = "forward"
     
-    def backward(self):
+    def reverse(self):
         """Moves PiBot backward"""
-        motor_a(BACKWARD)
-        motor_b(BACKWARD)
-        self.status = "backward"
+        motor_ctrl(MOTOR_A, BACKWARD)
+        motor_ctrl(MOTOR_B, BACKWARD)
+        self.status = "reverse"
 
     def left(self):
         """Moves PiBot left on a zero turn radius"""
-        motor_a(BACKWARD)
-        motor_b(FORWARD)
+        motor_ctrl(MOTOR_A, BACKWARD)
+        motor_ctrl(MOTOR_B, FORWARD)
         self.status = "zeroleft"
 
     def right(self):
         """Moves PiBot right on a zero turn radius"""
-        motor_a(FORWARD)
-        motor_b(BACKWARD)
+        motor_ctrl(MOTOR_A, FORWARD)
+        motor_ctrl(MOTOR_B, BACKWARD)
         self.status = "zeroright"
 
     def slight_left(self):
         """Moves PiBot gradually to the left"""
-        motor_b(FORWARD)
+        motor_ctrl(MOTOR_B, FORWARD)
         self.status = "slightleft"
 
     def slight_right(self):
         """Moves PiBot gradually to the right"""
-        motor_a(FORWARD)
+        motor_ctrl(MOTOR_A, FORWARD)
         self.status = "slightright"
 
     def stop(self):
         """Stops the PiBot"""
-        power_off()
+        power_ctrl(POWER_OFF)

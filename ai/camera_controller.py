b607f4432f3ddab57b43b3c1e38d3ff4c2639ac7
tsized <timsized@yahoo.com>
2014-04-14 20:36:25 -0400

    pushed picamera functions

--- ai/camera_controller.py
+++ ai/camera_controller.py
@@ -43,3 +43,24 @@ def remove_picture(filename):
     Removes filename
     """
     os.remove(filename)
+
+def timelapse(filename):
+    """
+    Takes a timelapse sequence of pictures.
+    """
+    with picamera.PiCamera() as cam:
+        cam.start_preview()
+        time.sleep(2)
+        for filename in cam.capture_continuous('img{counter:03d}.jpg'):
+            print('Captured %s' % filename)
+            time.sleep(60) # wait 1 minute
+
+def video(height, width):
+    """
+    Captures a video of specified height and width
+    """
+    with picamera.PiCamera() as cam:
+        cam.resolution = (height, width)
+        cam.start_recording('video.h264')
+        cam.wait_recording(60) # 1 minute video
+        cam.stop_recording()
